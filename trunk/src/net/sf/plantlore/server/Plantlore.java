4d8f03cc07e56f9c97fa87788b881e689a1a0b78
krater <krater@1e7d7472-c00b-0410-83ec-ca9128818323>
2006-03-30 21:06:39 +0000

    Command line server manager. This temporary solution allows the user to control the running server or start another server.
    Usage: java net.sf.plantlore.server.plantlore [command] [host] [port]
    where: command = start | stop | who | kick=id; default is `start`
           host = hostname (not IP); default is `localhost`
           port = port; default is `1099`
    
    Do not forget that once the server runs on a remote machine, the java.policy must be set adequately on both machines
    (SocketPermission, FilePermission)!
    
    Besides, the client must have classes needed to create stubs for SelectQuery, DBLayer.
    This can be ensured by generating stubs manually from SelectQueryImplementation and HibernateDBLayer (using the rmic compiler).

--- trunk/src/net/sf/plantlore/server/Plantlore.java
+++ trunk/src/net/sf/plantlore/server/Plantlore.java
@@ -1,8 +1,74 @@
 package net.sf.plantlore.server;
 
-public class Plantlore {
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Collection;
+
+
+/**
+ * Temporary solution.
+ * 
+ * @author Erik Kratochvíl (discontinuum@gmail.com)
+ * @since 30.3.2006
+ * @version beta not tested
+ */
+public class Plantlore {
+	
+	// Probably some more sophisticated method for obtaining a password from the user :)
+	public static String getPassword() {
+		return "poweroverwhelming";
+	}
+
+	/**
+	 * 
+	 * @param args Controlling the server from the command line:
+	 * 		start|stop|who|kick=id [host] [port]
+	 */
 	public static void main(String[] args) {
+		
+		int m = args.length, port = RMIServer.DEFAULT_PORT, id = -1;
+		String command = "start", host = null;
+		
+		// Parse the command line.
+		if(m >= 1) command = args[0];
+		if(m >= 2) try { int c = Integer.parseInt(args[1]); port = c; }
+			catch(NumberFormatException e) { host = args[1]; }
+		if(m >= 3) port = Integer.parseInt(args[2]);
+			
+		if(command.startsWith("kick")) {
+			id = Integer.parseInt(command.substring(5));
+			command = "kick";
+		}
+		 
+		// The server interface.		
+		Server server = null ;
+		
+		// Take the appropriate action.
+		try {
+			if (command.equalsIgnoreCase("start")) {
+				try { server = new RMIServer(port); } catch (Exception e) { System.err.println(e); server.stop(true); }
+			} 
+			else {
+				Registry registry = LocateRegistry.getRegistry(host, port);
+				Guard guard = (Guard) registry.lookup(Guard.ID);
+				server = guard.certify(getPassword());
+				if(server == null) { System.err.println("<!> No server on that port."); return; }
+				if(command.equalsIgnoreCase("stop")) server.stop(true);
+				else {
+					Collection<ConnectionInfo> clients = server.getClients();
+					int c = 0;
+					if(command.equalsIgnoreCase("who")) {
+						for(ConnectionInfo info : clients) System.out.println("(" + c++ + ") " + info);
+					}
+					else if(command.equalsIgnoreCase("kick")) {
+						for(ConnectionInfo info : clients) if(c++ == id) { server.disconnect(info); break; }
+					}
+					else System.err.println("<?> Unknown command.");
+				}
+					
+			}
+		} catch(Exception e) { System.err.println(e); }
 		
 	}
 
