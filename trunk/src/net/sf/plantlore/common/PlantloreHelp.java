5e6178bb91c1867711f809860bd5bbe5f002f405
kovo <kovo@1e7d7472-c00b-0410-83ec-ca9128818323>
2006-04-30 01:28:13 +0000

    Add help configuration (moved from net/sf/plantlore/config/help to net/sf/plantlore/help) and class for opening help viewer.

--- trunk/src/net/sf/plantlore/common/PlantloreHelp.java
+++ trunk/src/net/sf/plantlore/common/PlantloreHelp.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import javax.help.HelpSet;
 import javax.help.HelpSetException;
 import javax.help.JHelp;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 
 /**
@@ -24,10 +25,59 @@ public class PlantloreHelp {
     // File containing Java Help settings
     private static final String HELPFILE = "jhelpset.hs";
     
-    // Constants with help for author manager section
-    public static final String AUTHOR_MANAGER_GENERAL = "";
-    public static final String AUTHOR_MANAGER_ADD = "";
-    
+    // Basic information about Plantlore
+    public static final String PLANTLORE_OVERVIEW       = "plantlore.overview";
+    // Resources available for Plantlore users
+    public static final String PLANTLORE_RESOURCES      = "plantlore.resources";
+    // Text of licence used
+    public static final String PLANTLORE_LICENCE        = "plantlore.licence";
+    // Login description
+    public static final String LOGIN                    = "login";
+    // Description of selecting the database
+    public static final String SELECT_DB                = "login.selectdb";    
+    // Authentication description
+    public static final String AUTHENTICATION           = "login.authentication";    
+    // Description of settings dialog
+    public static final String SETTINGS                 = "settings";
+    // Main Plantlore window with occurrence overview    
+    public static final String PLANT_OVERVIEW           = "overview";    
+    // Adding and editing occurrense records
+    public static final String ADD_OCCURRENCE           = "overview.addoccurrence";
+    // Description of "basic data" section
+    public static final String ADD_BASIC                = "overview.addoccurrence.basic";    
+    // Description of "extended data" section
+    public static final String ADD_EXTENDED             = "overview.addoccurrence.extended";
+    // Description of checklist
+    public static final String ADD_CHECKLIST            = "overview.addoccurrence.checklist";        
+    // Searching the occurrence records
+    public static final String SEARCH_OCCURRENCE        = "overview.search";
+    // History for the selected occurrence record
+    public static final String OCCURRENCE_HISTORY       = "overview.history";    
+    // Description of author manager
+    public static final String AUTHOR_MANAGER           = "author";
+    // Add /edit author dialog
+    public static final String ADD_AUTHOR               = "author.add";        
+    // Description of publication manager
+    public static final String PUBLICATION_MANAGER      = "publication";    
+    // Add/edit publication dialog
+    public static final String PUBLICATION_ADD          = "publication.add";        
+    // Description of metadata manager
+    public static final String METDATA_MANAGER          = "metdata";    
+    // Add/edit metadata dialog
+    public static final String METDATA_ADD              = "metdata.add";        
+    // Description of history manager
+    public static final String HISTORY_MANAGER          = "history";
+    // Desctripton of user manager
+    public static final String USER_MANAGER             = "user";    
+    // Add/edit user dialog
+    public static final String USER_ADD                 = "user.add";    
+    // Modification of rights description
+    public static final String USER_RIGHTS              = "user.rights";    
+    // Import feature description
+    public static final String IMPORT                   = "import";
+    // Export feature description
+    public static final String EXPORT                   = "export";   
+            
     /** Creates a new instance of PlantloreHelp */
     public PlantloreHelp() {
         
@@ -38,7 +88,6 @@ public class PlantloreHelp {
      *
      * @param section Section of help which will be shown. Sections are defined in 
      *                <code>jHelpMap.jhm</code> and <code>jHelpToc.xml</code>
-     * @throws ???
      */
     public static void showHelp(String section) {
         JHelp helpViewer;
@@ -52,7 +101,8 @@ public class PlantloreHelp {
             // Set the initial entry point in the table of contents.
             helpViewer.setCurrentID(section);
         } catch (HelpSetException e) {
-            // TODO: Throw new exception, we should define new exception
+            System.out.println("EXCEPTION: "+e.getMessage());
+            e.printStackTrace();
             return;
         }
         // Create a new frame.
@@ -62,6 +112,5 @@ public class PlantloreHelp {
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         // Make the frame visible.
         frame.setVisible(true);        
-    }
-    
+    }    
 }
