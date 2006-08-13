1323582459ad6aba0585e8dd0b011947ec8b9f95
krater <krater@1e7d7472-c00b-0410-83ec-ca9128818323>
2006-08-13 21:25:01 +0000

    Ok. Delete those unused obsolete files... (Sorry for the amount of commits but the SVN client prevents me from recursive deletion of directories).

--- trunk/src/net/sf/plantlore/client/imports/table/DataHolder.java
+++
@@ -1,9 +0,0 @@
-package net.sf.plantlore.client.tableimport;
-
-import net.sf.plantlore.common.record.Record;
-
-public class DataHolder {
-	public TableParser.Action action;
-	public Record record;
-	public Record replacement;
-}
