361b7d42100b40f9538022f3814cc518a3beb3d8
fraktalek <fraktalek@1e7d7472-c00b-0410-83ec-ca9128818323>
2006-08-25 23:31:35 +0000

    Easier to use implementation of the escape key behaviour. Simply call new DefaultEscapeKeyPressed(view) anytime after the view already called it's initComponents() method. Nothing more's needed. Also supports custom AbstractActions.

--- trunk/src/net/sf/plantlore/common/DefaultEscapeKeyPressed.java
+++ trunk/src/net/sf/plantlore/common/DefaultEscapeKeyPressed.java
@@ -1,7 +1,10 @@
 package net.sf.plantlore.common;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.AbstractAction;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 
@@ -9,10 +12,17 @@ import javax.swing.JFrame;
  * KeyListener class controlling the pressing key ESCAPE. On key ESCAPE hides the view dialog.   
  * <br/>
  * The use is very simple (in the controller):
+ *
+ * Just put the following line after the dialog's or frame's initComponents() method:
  * <br/>
- * <code>myView.myButton.addKeyListener( new DefaultEscapeKeyPressed( myView ) ); </code>
+ * <code>new DefaultEscapeKeyPressed(this)</code>
  * <br/>
  * 
+ *  This class installs itself recursively into each component of the dialog or frame.
+ *
+ *  You can also use the constructors with AbstractAction. In that case this listener won't simply hide the dialog or frame
+ * but will call yourAction.actionPerformed(null). This is handful if your cancel action is more than just hiding the dialog.
+ *
  *   @author Lada Oberreiterova
  *   @version 1.0
  */  
@@ -20,13 +30,22 @@ public class DefaultEscapeKeyPressed implements KeyListener {
 
 	protected JDialog dialog;
 	protected JFrame frame;
-	
+	private AbstractAction action;
+        
+        private void installMe(Component component) {
+            component.addKeyListener(this);
+            if (component instanceof Container)
+                for (Component c : ((Container)component).getComponents() )
+                    installMe(c);            
+        }
+        
 	/**
 	 * Create new instance of DefaultEscapeKeyPressed
 	 * @param dialogView 
 	 */
 	public 	DefaultEscapeKeyPressed(JDialog dialogView) {
 		dialog = dialogView;		
+                installMe(dialog);
 	}
 	
 	/**
@@ -34,7 +53,28 @@ public class DefaultEscapeKeyPressed implements KeyListener {
 	 * @param frameView
 	 */
 	public 	DefaultEscapeKeyPressed(JFrame frameView) {
-		frame = frameView;		
+		frame = frameView;	
+                installMe(frame);
+	}
+	
+	/**
+	 * Create new instance of DefaultEscapeKeyPressed
+	 * @param dialogView 
+	 */
+	public 	DefaultEscapeKeyPressed(JDialog dialogView, AbstractAction action) {
+            this.action = action;
+		dialog = dialogView;		
+                installMe(dialog);
+	}
+	
+	/**
+	 * Create new instance of DefaultEscapeKeyPressed
+	 * @param frameView
+	 */
+	public 	DefaultEscapeKeyPressed(JFrame frameView, AbstractAction action) {
+            this.action = action;
+		frame = frameView;	
+                installMe(frame);
 	}
 	
 	/**
@@ -42,7 +82,12 @@ public class DefaultEscapeKeyPressed implements KeyListener {
 	 * @param evt
 	 */
  	public void keyPressed(KeyEvent evt){
- 		if (evt.getKeyCode() == KeyEvent.VK_ESCAPE) { 			
+ 		if (evt.getKeyCode() == KeyEvent.VK_ESCAPE) { 
+                    if (action != null) {
+                        action.actionPerformed(null);
+                        return;
+                    }
+                    
  			if (dialog != null) {
  				dialog.setVisible(false);
  			} else if (frame != null) {
